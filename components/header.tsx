f0532ccc91b2c8ca1492432c0ae1713e0d92df4a
Vaishnav <vaishnavvaichu123@gmail.com>
2025-04-21 13:16:19 +0530

    added home button

--- components/header.tsx
+++ components/header.tsx
@@ -39,8 +39,16 @@ export default function Header({ title, children }: HeaderProps) {
           </button>
         )}
         <h1 className="text-lg font-semibold md:text-xl">{getPageTitle()}</h1>
+        
       </div>
       <div className="flex items-center gap-2">{children}</div>
+      {showBackButton &&  (
+          <button
+          onClick={() => router.push('/dashboard')}
+          className="flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-sm font-medium text-gray-600 shadow-sm hover:bg-gray-200 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+          >
+          <span className="font-black text-lg">⌂</span>
+          </button>)}
     </header>
   )
 }
