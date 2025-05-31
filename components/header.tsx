2f7cfa998bf4c14e96d8c35fc751de23945fecf8
Vaishnav <vaishnavvaichu123@gmail.com>
2025-05-31 10:48:09 +0530

    added reload button

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,6 @@
 import type React from "react"
 import { usePathname, useRouter } from "next/navigation"
 
-
 interface HeaderProps {
   title: string
   children?: React.ReactNode
@@ -31,7 +30,7 @@ export default function Header({ title, children }: HeaderProps) {
     <header className="sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b bg-white px-4 md:px-6">
       <div className="flex items-center gap-4">
         {showBackButton && (
-         <button
+          <button
             onClick={() => router.back()}
             className="flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-sm font-medium text-gray-600 shadow-sm hover:bg-gray-200 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
           >
@@ -39,17 +38,27 @@ export default function Header({ title, children }: HeaderProps) {
           </button>
         )}
         <h1 className="text-lg font-semibold md:text-xl ml-14 md:ml-0">{getPageTitle()}</h1>
-        
       </div>
-      <div className="flex items-center gap-2">{children}</div>
-      {showBackButton &&  (
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          {children}
           <button
-          onClick={() => router.push('/dashboard')}
-          className="flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-sm font-medium text-gray-600 shadow-sm hover:bg-gray-200 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+            onClick={() => window.location.reload()}
+            className="flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-sm font-medium text-gray-600 shadow-sm hover:bg-gray-200 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
           >
-          <span className="font-black text-lg">⌂</span>
-          </button>)}
+            <span className="font-black text-lg">⟳</span>
+          </button>
+        </div>
+        {showBackButton && (
+          <button
+            onClick={() => router.push('/dashboard')}
+            className="flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-gray-100 text-sm font-medium text-gray-600 shadow-sm hover:bg-gray-200 hover:text-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
+          >
+            <span className="font-black text-lg">⌂</span>
+          </button>
+        )}
+      </div>
+
     </header>
   )
 }
-
